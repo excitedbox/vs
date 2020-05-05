679a943f5069894df4c28e9fac52aae4520cab9a
maldan <blackwanted@yandex.ru>
2020-05-05 09:32:30 +0300

    Update

--- src/lib/extender/HTMLExtender.ts
+++ src/lib/extender/HTMLExtender.ts
@@ -11,7 +11,7 @@ declare global {
 
         off(eventList, fn);
 
-        setDraggable();
+        setDraggable(params?: {});
     }
 
     interface Document {
@@ -23,15 +23,15 @@ declare global {
     }
 }
 
-let __onceCheckMap: Map<any, Array<string>> = new Map<any, Array<string>>();
+const __onceCheckMap: Map<any, string[]> = new Map<any, string[]>();
 
 Document.prototype.once = Element.prototype.once = function (eventList, fn, params?) {
-    if (!__onceCheckMap.has(this)) __onceCheckMap.set(this, []);
+    if (!__onceCheckMap.has(this)) {__onceCheckMap.set(this, []);}
     eventList = eventList.split(' ');
 
     for (let i = 0; i < eventList.length; i++) {
         // Check if event already exists
-        if (__onceCheckMap.get(this).includes(eventList[i])) continue;
+        if (__onceCheckMap.get(this).includes(eventList[i])) {continue;}
         __onceCheckMap.get(this).push(eventList[i]);
 
         // Add event once
@@ -40,7 +40,7 @@ Document.prototype.once = Element.prototype.once = function (eventList, fn, para
 };
 
 Document.prototype.on = Element.prototype.on = function (eventList, fn, params?) {
-    let list = eventList.split(' ');
+    const list = eventList.split(' ');
     for (let i = 0; i < list.length; i++) {
         if (list[i] === `hold`) {
             let holdTimer;
@@ -59,22 +59,24 @@ Document.prototype.on = Element.prototype.on = function (eventList, fn, params?)
 };
 
 Document.prototype.off = Element.prototype.off = function (eventList, fn) {
-    let list = eventList.split(' ');
+    const list = eventList.split(' ');
     for (let i = 0; i < list.length; i++)
-        this.removeEventListener(list[i], fn);
+        {this.removeEventListener(list[i], fn);}
 
     return fn;
 };
 
-HTMLElement.prototype.setDraggable = function () {
-    let element: HTMLElement = this;
+HTMLElement.prototype.setDraggable = function (params: { positionType: string; onDrag?: Function } = {
+    positionType: 'relative'
+}): void {
+    const element: HTMLElement = this;
 
-    element.style.position = 'relative';
+    element.style.position = params.positionType;
     let startPos = {
         x: element.getBoundingClientRect().left,
         y: element.getBoundingClientRect().top
     };
-    let startSize = {
+    const startSize = {
         width: element.getBoundingClientRect().width,
         height: element.getBoundingClientRect().height
     };
@@ -88,31 +90,37 @@ HTMLElement.prototype.setDraggable = function () {
             x: e.pageX - ~~Number.parseInt(element.style.left),
             y: e.pageY - ~~Number.parseInt(element.style.top)
         };
-        let touches = e.changedTouches || [];
+        const touches = e.changedTouches || [];
         if (touches.length > 1) {
-            let a = touches[0].pageX - touches[1].pageX;
-            let b = touches[0].pageY - touches[1].pageY;
+            const a = touches[0].pageX - touches[1].pageX;
+            const b = touches[0].pageY - touches[1].pageY;
             startScale = Math.sqrt(a * a + b * b) - currentScale;
         }
         return false;
     }, {passive: false});
+
     document.on('mousemove touchmove', (e) => {
-        if (!isDrag) return false;
+        if (!isDrag) {return false;}
         e.preventDefault();
-        let touches = e.changedTouches || [];
+        const touches = e.changedTouches || [];
         if (touches.length > 1) {
-            let a = touches[0].pageX - touches[1].pageX;
-            let b = touches[0].pageY - touches[1].pageY;
+            const a = touches[0].pageX - touches[1].pageX;
+            const b = touches[0].pageY - touches[1].pageY;
             currentScale = Math.sqrt(a * a + b * b) / 100;
 
             element.style.transform = `scale(${currentScale})`;
         } else {
             element.style.left = e.pageX - startPos.x + 'px';
             element.style.top = e.pageY - startPos.y + 'px';
+
+            if (params.onDrag) {
+                params.onDrag(e.pageX - startPos.x, e.pageY - startPos.y);
+            }
         }
 
         return false;
     }, {passive: false});
+
     document.on('mouseup touchend', (e) => {
         isDrag = false;
         return false;
@@ -121,14 +129,14 @@ HTMLElement.prototype.setDraggable = function () {
 
 HTMLImageElement.prototype.toJPEG = function () {
     return new Promise((resolve => {
-        let canvas = document.createElement('canvas');
+        const canvas = document.createElement('canvas');
         canvas.setAttribute("width", this.width);
         canvas.setAttribute("height", this.height);
         canvas.style.position = 'absolute';
         canvas.style.visibility = 'hidden';
         document.querySelector('body').appendChild(canvas);
 
-        let ctx = canvas.getContext('2d');
+        const ctx = canvas.getContext('2d');
         ctx.imageSmoothingQuality = "high";
         ctx.drawImage(this, 0, 0, this.width, this.height);
         canvas.toBlob((blob) => {
